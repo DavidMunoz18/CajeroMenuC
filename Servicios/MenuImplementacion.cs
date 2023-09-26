deeb589b4ce4b4c21c0af5aff4cb401dd99a40a8
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2023-09-26 14:15:37 +0200

    Agregar archivos de proyecto.

---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroMenuC.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public void mostrarMensajeBienvenida()
+        {
+            string mensaje = "Banco CSI1";
+                Console.WriteLine(mensaje);
+        }
+
+        public int mostrarMenuYSeleccion()
+        {
+            int opcionIntroducida;
+            Console.WriteLine("0. Cerrar aplicación");
+            Console.WriteLine("1 Darse de alta ");
+            Console.WriteLine("2. Ingresar dinero");
+            Console.WriteLine("3. Sacar dinero");
+            Console.WriteLine("4. Transferir dinero");
+            Console.WriteLine("5. Historial de operaciones");
+            Console.WriteLine("########################");
+            Console.WriteLine("Seleccione una opción");
+
+            opcionIntroducida = Console.ReadKey(true).KeyChar-('0');
+           
+            return opcionIntroducida;
+
+              
+        }
+    }
+}
---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroMenuC.Servicios
+{
+    /// <summary>
+    /// Interfaz con la relación de métodos de la funcionalidad menú
+    /// </summary>
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// Método que muestra mensaje de bienvenida al cajero
+        /// 250923 - dmp
+        /// </summary>
+        void mostrarMensajeBienvenida();
+        /// <summary>
+        /// Método que muestra el menu y recoge la selección del ususario
+        /// 260923 - dmp
+        /// </summary>
+        /// <returns></returns>
+        public int mostrarMenuYSeleccion();
+    }
+}
